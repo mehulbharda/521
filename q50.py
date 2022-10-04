06e3d9dc4519ee2fdaf5a208750a66fd70f69bcd
mehulbharda <61320410+mehulbharda@users.noreply.github.com>
2022-10-04 19:40:15 -0400

    Update q50.py

--- q50.py
+++ q50.py
@@ -3,3 +3,15 @@
 #(2) extract all the vowels in the input string and make them lower case 
 #(3) add all the nonduplicate vowels(aeiou) and 
 #(4) print the number of trials that you did to collect the characters.
+#Q50
+
+value = "Enter a string"
+vowels_1 = "a,e,i,o,u,A,E,I,O,U"
+vowels_2 = "a,e,i,o,u"
+vowels_len = len(vowels)
+
+temp_str = ""
+
+for e in value:
+    if e in vowels:
+        temp_str = temp_str + e.lower()
