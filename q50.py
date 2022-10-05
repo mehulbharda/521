f0e4c1fc327835d2e0770222ccc9d0ab670ae75f
mehulbharda <mehul.bharda@gmail.com>
2022-10-04 21:03:13 -0400

    mehul final update

--- q50.py
+++ q50.py
@@ -11,22 +11,25 @@ value_lowercase= value.lower() # check syntax
 
 print(value_lowercase)
 
-vowels = "a,e,i,o,u"
+vo = "a,e,i,o,u"
 
 count=[0,0,0,0,0]
 
 
 temp_str = ""
+ts = ""
 
 for e in value_lowercase:
-    if e in vowels:
-        x=vowels.index(e)
+    if e in vo:
+        ts = ts + e
+        x=int(vo.index(e)/2)
         print(x)
         count[x]=count[x]+1
+print(ts)
 
 
 for count, ite in enumerate(count):
     if ite == 1:
-        temp_str = temp_str + vowels[count]
+        temp_str = temp_str + vo[count*2]
 
 print(temp_str)
