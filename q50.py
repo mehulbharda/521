ecdf21e689efe6f17c5264260db7cb0e900fe4e8
pqmzla1029 <igormonteiro101@gmail.com>
2022-10-04 20:22:59 -0400

    Update #3

--- q50.py
+++ q50.py
@@ -8,15 +8,18 @@
 value = input("Enter a string")
 value_lowercase= value.lower() # check syntax
 
-vowels_1 = "a,e,i,o,u" # don't need to check capital alphabets
-vowels_2 = "a,e,i,o,u"
+print(value_lowercase)
+
+vowels = "a,e,i,o,u"
+
 count=[0,0,0,0,0]
-vowels_len = len(vowels_2)
+
+vowels = len(vowels_2)
 
 temp_str = ""
 
 for e in value_lowercase:
-    if e in vowels_2:
-        x=vowels_2.index(e)
+    if e in vowels:
+        x=vowels.index(e)
         count[x]=count[x]+1
         temp_str = temp_str + e
