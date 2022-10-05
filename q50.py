ac17fc6944fb52573a4e33b8f50aad97543996a8
pqmzla1029 <igormonteiro101@gmail.com>
2022-10-04 20:00:55 -0400

    Update q50.py

--- q50.py
+++ q50.py
@@ -1,19 +1,22 @@
-#Develop a Python program that will : 
-#(1) prompt for the input 
-#(2) extract all the vowels in the input string and make them lower case 
-#(3) add all the nonduplicate vowels(aeiou) and 
-#(4) print the number of trials that you did to collect the characters.
+#Develop a Python program that will :
+#(1) prompt for the input
+#(2) extract all the vowels in the input string and make them lower case
+#(3) add all the nonduplicate vowels(aeiou) and
+#(4) print the number of trials that you did to collect the 5 vowels .
 #Q50
 
 value = "Enter a string"
-value_lowercase= value.toLowerCase() # check syntax
+value_lowercase= value.lower() # check syntax
 
 vowels_1 = "a,e,i,o,u,A,E,I,O,U" # don't need to check capital alphabets
 vowels_2 = "a,e,i,o,u"
-vowels_len = len(vowels)
+count=[0,0,0,0,0]
+vowels_len = len(vowels_2)
 
 temp_str = ""
 
 for e in value_lowercase:
-    if e in vowels:
-        temp_str = temp_str + e.lower()
+    if e in vowels_2:
+        x=vowels_2.index(e)
+        count[x]=count[x]+1
+        temp_str = temp_str + e
